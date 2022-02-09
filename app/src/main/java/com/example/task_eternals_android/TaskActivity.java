56f653bf203988f0f3538acaf84c9602551358f9
NiharikaReddyMainampati <C0836194@mylambton.ca>
2022-02-09 09:01:17 -0500

    Intent has been added to navigate to add new task

--- app/src/main/java/com/example/task_eternals_android/TaskActivity.java
+++ app/src/main/java/com/example/task_eternals_android/TaskActivity.java
@@ -2,17 +2,27 @@ package com.example.task_eternals_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class TaskActivity extends AppCompatActivity {
+    private Button addTask;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+        addTask = findViewById(R.id.addTaskBtn);
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TaskActivity.this,AddNewTask.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
