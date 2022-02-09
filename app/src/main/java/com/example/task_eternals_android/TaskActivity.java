55adcace2faaab66cb8bb5d8d21fcd75dfad0d70
NiharikaReddyMainampati <C0836194@mylambton.ca>
2022-02-09 02:09:20 -0500

    XML Code is Written for adding new task in activity_add_new_task.xml file.

--- app/src/main/java/com/example/task_eternals_android/TaskActivity.java
+++ app/src/main/java/com/example/task_eternals_android/TaskActivity.java
@@ -3,12 +3,19 @@ package com.example.task_eternals_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TaskActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+
+
     }
+
+
 }
