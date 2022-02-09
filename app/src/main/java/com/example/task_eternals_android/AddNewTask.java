c99a3a5161f7ec548e9909af8fa1d7202208a952
NiharikaReddyMainampati <C0836194@mylambton.ca>
2022-02-09 09:21:19 -0500

    DatePicker Dialog has been added for choosing due date while creating new task

--- app/src/main/java/com/example/task_eternals_android/AddNewTask.java
+++ app/src/main/java/com/example/task_eternals_android/AddNewTask.java
@@ -2,13 +2,51 @@ package com.example.task_eternals_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class AddNewTask extends AppCompatActivity {
+    private DatePickerDialog datePickerDialog;
+    private TextView tvTimer, tvDates;
+    int tHour, tMinute;
+    DatePickerDialog.OnDateSetListener setListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_task);
+        tvTimer = findViewById(R.id.tvTime);
+        tvDates = findViewById(R.id.tvDate);
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+        tvDates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        AddNewTask.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth
+                        ,setListener,year,month,day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                month = month+1;
+                String date = dayOfMonth+"/"+month+"/"+year;
+                tvDates.setText(date);
+            }
+        };
     }
 }
